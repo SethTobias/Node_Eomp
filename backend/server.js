7cb3348440ef64e52a0ea83c31a15bcfb263b9db
SethTobias <seth.addams1107@gmail.com>
2024-03-01 09:16:02 +0200

    cor change

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,16 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
+app.use( (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Request-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Expose-Headers", "Authorization");
+  next();
+});
+
 // Applying CORS middleware to the Express app with the specified options
 app.use(cors(corsOptions));
 
